82d72b825afd056b869fd3dcb57b9c5ae067bf44
Ashley <aforrest56@gmail.com>
2022-08-01 21:17:18 -0500

    Part 1 Done

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,9 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
+  photosTitle = 'Favorite Photos';
+  image1 = 'https://www.orlandomagazine.com/content/uploads/2021/09/l/s/wdw-50-character-fashions-1-scaled-1.jpg';
+  image2 = 'https://livingbydisney.com/wp-content/uploads/2021/09/WDW-50th-Anniversary.jpg';
   image3 = '';
 
   constructor() { }
